44acf6d72af87ac33574a2645c2bc67d2f10026c
Nikolai <artemyev1000@gmail.com>
2022-09-02 19:41:06 +0900

    Hallelujah)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)  {
+
+
+        String Input = new String();
+        String result = calc (Input);
+        System.out.println(result);
+
+
+
+        }
+
+    public  static String calc (String Input) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int sum = num1+num2;
+
+       String result = Integer.toString(sum);
+
+        return result;
+
     }
-}
+
+    }
